23346e1eab8c1df66070fa0b7caa05f3aea34ed1
Maksim Mozhorov <Maksim_Mozhorov@epam.com>
2019-10-30 20:19:44 +0300

    change get new users

--- app/routes/newUsers.js
+++ app/routes/newUsers.js
@@ -20,13 +20,12 @@ module.exports =  getNewUsers = (app) =>{
                         const usersJsonObject = [];
                         for(let i = (limit*offset); (i < usersFromCsv.length) && i < (limit*(1+Number(offset))); i++){
                             const user = usersFromCsv[i];
-                            const finishedInfo = {};
-                            for( let j = 0; j < csvRow[0].length; j++ ){
-                                finishedInfo[csvRow[0][j]] = user[j];
-                            }
-                            usersJsonObject.push(finishedInfo);
+                            usersJsonObject.push({
+                                "name": user[0],
+                                "lastName": user[1],
+                                "join_date": user[5]
+                            });
                         }
-
                         response.send({
                             "users": usersJsonObject
                         });

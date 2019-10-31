a417b9d701dc40508323b5c5bf448fc65818b966
Maksim Mozhorov <Maksim_Mozhorov@epam.com>
2019-10-31 11:01:45 +0300

    change get new users

--- app/actions/loadFileFromDropbox.js
+++ app/actions/loadFileFromDropbox.js
@@ -1,15 +1,9 @@
 const csv=require('csvtojson');
 const config = require('../../config');
 
-module.exports = ( resolve, reject) => {
-    return config.dropbox.DropboxWebApi.filesDownload({path: config.dropbox.pathToReports})
-            .then( resp => {
-                const users = resp.fileBinary.toString();
-                csv({noheader:true, output: "csv"})
-                    .fromString(users)
-                        .then((csvRow)=> csvRow)
-                        .catch( error => reject(error) )
-                .then( (csvRow)=> csvRow )
-            })
-        .catch( error => console.error(error) );
+module.exports = async ( resolve, reject) => {
+    const csvFile = await config.dropbox.DropboxWebApi.filesDownload({path: config.dropbox.pathToReports});
+    const users = await csvFile.fileBinary.toString();
+    const csvRow = await csv({noheader:true, output: "csv"}).fromString(users);
+    return csvRow;
 }
--- app/routes/newUsers.js
+++ app/routes/newUsers.js
@@ -2,6 +2,20 @@ const csv=require('csvtojson');
 const config = require('../../config');
 const loadFileFromDropbox = require('../actions/loadFileFromDropbox');
 
+const getFilteredUsers = (csvRow, limit, offset) => {
+    const usersFromCsv =  csvRow.slice(1);
+    const usersJsonObject = [];
+    for(let i = (limit*offset); (i < usersFromCsv.length) && i < (limit*(1+Number(offset))); i++){
+        const user = usersFromCsv[i];
+        usersJsonObject.push({
+            "name": user[0],
+            "lastName": user[1],
+            "join_date": user[5]
+        });
+    }
+    return usersJsonObject;
+};
+
 module.exports =  getNewUsers = (app) =>{
     app.post('/getNewUsers', (request, response) => {
         const {
@@ -9,17 +23,9 @@ module.exports =  getNewUsers = (app) =>{
             offset = 0
         } = request.body;
         const CSVFILE = loadFileFromDropbox();
-            CSVFILE.then((csvRow) => {
-                const usersFromCsv =  csvRow.slice(1);
-                const usersJsonObject = [];
-                for(let i = (limit*offset); (i < usersFromCsv.length) && i < (limit*(1+Number(offset))); i++){
-                    const user = usersFromCsv[i];
-                    usersJsonObject.push({
-                        "name": user[0],
-                        "lastName": user[1],
-                        "join_date": user[5]
-                    });
-                }
+
+        CSVFILE.then((csvRow) => {
+                const usersJsonObject = getFilteredUsers(csvRow, limit, offset);
                 response.send({
                     "users": usersJsonObject
                 });
@@ -31,50 +37,3 @@ module.exports =  getNewUsers = (app) =>{
 
     });
 };
-
-
-
-
-
-/*
-const csv=require('csvtojson');
-const config = require('../../config');
-
-module.exports =  getNewUsers = (app) =>{
-    app.post('/getNewUsers', (request, response) => {
-        const {
-            limit = 20,
-            offset = 0
-        } = request.body;
-        config.dropbox.DropboxWebApi.filesDownload({path: config.dropbox.pathToReports})
-            .then( resp => {
-                const users = resp.fileBinary.toString();
-                csv({
-                    noheader:true,
-                    output: "csv"
-                })
-                    .fromString(users)
-                    .then((csvRow)=>{
-                        const usersFromCsv =  csvRow.slice(1);
-                        const usersJsonObject = [];
-                        for(let i = (limit*offset); (i < usersFromCsv.length) && i < (limit*(1+Number(offset))); i++){
-                            const user = usersFromCsv[i];
-                            usersJsonObject.push({
-                                "name": user[0],
-                                "lastName": user[1],
-                                "join_date": user[5]
-                            });
-                        }
-                        response.send({
-                            "users": usersJsonObject
-                        });
-                    })
-            })
-            .catch( error => {
-                console.error(error);
-                response.send("Something went wrong")
-            });
-
-    });
-};
- */

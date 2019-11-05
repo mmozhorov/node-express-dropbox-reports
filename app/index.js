5309b48049397de0805395e1ecb8b439c2e83ba4
Maksim_Mozhorov <Maksim_Mozhorov@epam.com>
2019-11-05 15:56:59 +0300

    Changed config

--- app/index.js
+++ app/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const config = require('../config');
-
 const app = express();
 
 app.use(bodyParser.json());
--- config/dropbox.js
+++ config/dropbox.js
@@ -1,7 +1,6 @@
 const access_token = require("./db.js").access_token;
 const pathToReports = require("./db.js").pathToReports;
 const Dropbox = require('dropbox').Dropbox;
-const csvtojsonV2=require("csvtojson");
 require('isomorphic-fetch');
 
 const DropboxWebApi = new Dropbox({ accessToken: access_token });
@@ -10,10 +9,3 @@ module.exports = {
     DropboxWebApi,
     pathToReports
 };
-
-// config.dropbox.DropboxWebApi.filesDownload({path: config.dropbox.pathToReports})
-//     .then( response => {
-//         const users = response.fileBinary.toString();
-//         console.log(users);
-//     })
-//     .catch( error => console.error(error));

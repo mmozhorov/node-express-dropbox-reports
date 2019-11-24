7d324dd6c3c9f196a82b1f26648e19a7d1883a71
Maksim Mozhorov <Maksim_Mozhorov@epam.com>
2019-11-24 22:26:30 +0300

    Finished refactor

--- src/resourses/users/topSalaries/mapper.js
+++ src/resourses/users/topSalaries/mapper.js
@@ -7,20 +7,10 @@ module.exports = async (csvRow = []) => {
         return [];
     }
     const currentCurrency = await getCurrency();
-    let usersFromCsv =  csvRow.slice(1);
-    usersFromCsv = usersFromCsv.sort(sortBySalary);
-    const usersJsonObject = [];
-    for(let i = 0; i < 10; i++){
-        if (!usersFromCsv[i]){
-            break;
-        }
-        const user = usersFromCsv[i];
-        usersJsonObject.push({
-            "name": user[0],
-            "lastName": user[1],
-            "salary, RUB": Number(user[2]*currentCurrency.data.Valute["USD"]["Value"]).toFixed(2) + "Р",
-            "salary, USD": "$" + Number(user[2]),
-        });
-    }
-    return usersJsonObject.filter( user => user.name);
+    return csvRow.slice(1).sort(sortBySalary).map( user => ({
+        "name": user[0],
+        "lastName": user[1],
+        "salary, RUB": Number(user[2]*currentCurrency.data.Valute["USD"]["Value"]).toFixed(2) + "Р",
+        "salary, USD": "$" + Number(user[2]),
+    })).filter( user => user.name);
 };
--- src/resourses/users/topSalaries/service.js
+++ src/resourses/users/topSalaries/service.js
@@ -2,17 +2,19 @@ const loadFileFromDropbox = require('../../../common/utils/loadFileFromDropbox')
 const getSalaries = require('./mapper');
 const newUsersValidation = require('../../../validation/newUsersValidation');
 const errorResponse = require('../../../common/utils/errorsHandler');
+const paginationFilter = require('../../../common/utils/paginationFilter');
 
 module.exports = async (request, response) => {
     newUsersValidation("newUsersRequestSchema")(request, response);
     try{
         const csvRow = await loadFileFromDropbox();
-        const usersJsonObject = await getSalaries(csvRow);
+        const users = await getSalaries(csvRow);
+        const usersJsonObject = paginationFilter(users, request.body);
         response.status(200).json({
             "users": usersJsonObject
         });
     }
     catch(error){
-        errorResponse(response, error)
+        errorResponse(response, error);
     }
 };

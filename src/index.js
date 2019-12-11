aa8a53c1fe79e67644ca599688feb7e259ef91fe
Maksim_Mozhorov <Maksim_Mozhorov@epam.com>
2019-12-11 18:15:32 +0300

    Changed structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorHandler = require('./common/utils/errorsHandler');
 const log4js = require('log4js');
 const app = express();
 const jwt = require('../config/jwt');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../config/swagger.json');
 
 app.use(log4js.connectLogger(log4js.getLogger("http"), { level: 'auto' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', require('swagger-ui-express').serve, require('swagger-ui-express').setup(require('../config/swagger.json')));
 app.use('/auth', require('./resourses/auth/router'));
 
 app.use(jwt());
@@ -19,6 +16,6 @@ app.use(jwt());
 app.use('/reports', require("./resourses/reports/router") );
 app.use('/user', require("./resourses/user/router"));
 
-app.use(errorHandler);
+app.use(require('./common/utils/errorsHandler'));
 
 module.exports = app;
